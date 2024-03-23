d246087294515ba0002fcaf21f6718cb2eb78e68
noorasif17 <162204402+noorasif17@users.noreply.github.com>
2024-03-23 16:59:04 -0600

    publish my calculator

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstnumber" },
-    { message: "Enter second number", type: "number", name: "secondnumber" },
+    { message: "Enter first number:", type: "number", name: "firstnumber" },
+    { message: "Enter second NUmber:", type: "number", name: "secondnumber" },
     {
-        message: "Select one of the operators to perform operation",
+        message: "Select one of the operator to perform operation:",
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
-//console.log(answer);
 if (answer.operator === "Addition") {
-    console.log(answer.firstnumber + answer.secondnumber);
+    console.log("The answer is : ", answer.firstnumber + answer.secondnumber);
 }
 else if (answer.operator === "Subtraction") {
-    console.log(answer.firstnumber - answer.secondnumber);
+    console.log("The answer is : ", answer.firstnumber - answer.secondnumber);
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.firstnumber * answer.secondnumber);
+    console.log("The answer is : ", answer.firstnumber * answer.secondnumber);
 }
 else if (answer.operator === "Division") {
-    console.log(answer.firstnumber / answer.secondnumber);
+    console.log("The answer is : ", answer.firstnumber / answer.secondnumber);
 }
 else {
-    ("Please select valid operator");
+    console.log("please select valid operator");
 }
+console.log("ThHE END!");
--- index.ts
+++ index.ts
@@ -1,28 +1,31 @@
-#! usr/bin/env node
-import inquirer from "inquirer"
-const answer =await inquirer.prompt([
-    { message : "Enter first number", type: "number", name: "firstnumber" },
-    { message : "Enter second number", type: "number", name: "secondnumber" },
-    {
-        message : "Select one of the operators to perform operation",
-        type : "list",
-        name: "operator",
-        choices : ["Addition","Subtraction", "Multiplication", "Division"],
-     },
-    ]);
-    //console.log(answer);
-    if (answer.operator === "Addition") {
-        console.log(answer.firstnumber + answer.secondnumber);
-    }
-    else if (answer.operator === "Subtraction") {
-        console.log(answer.firstnumber - answer.secondnumber);
-    }
-    else if(answer.operator === "Multiplication") {
-        console.log(answer.firstnumber * answer.secondnumber);
-    }
-    else if(answer.operator === "Division") {
-        console.log(answer.firstnumber / answer.secondnumber);
-    }
-    else{
-        ("Please select valid operator")
-    }
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter first number:",type: "number", name: "firstnumber" },
+  { message: "Enter second NUmber:",type: "number", name: "secondnumber" },
+  {
+    message: "Select one of the operator to perform operation:",
+    type: "list",
+    name: "operator",
+    choices: ["Addition","Subtraction","Multiplication", "Division"],
+  },
+]);
+
+if(answer.operator ==="Addition"){
+    console.log("The answer is : " , answer.firstnumber + answer.secondnumber);
+
+}else if(answer.operator === "Subtraction"){
+    console.log("The answer is : " , answer.firstnumber - answer.secondnumber);
+
+}else if(answer.operator=== "Multiplication"){
+    console.log("The answer is : " , answer.firstnumber * answer.secondnumber);
+
+}else if(answer.operator === "Division"){
+    console.log("The answer is : " , answer.firstnumber / answer.secondnumber);
+
+}
+else{
+    console.log("please select valid operator");
+}
+console.log("ThHE END!")
